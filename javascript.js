24c42925a7ce11d349dc76f7d7c5eb7e0946700d
itstoomanymichaels <maikeruh03@gmail.com>
2022-09-01 16:02:20 -0400

    Forgot to log commits, alot has been done. Template for webpage finished, currently adding javascript. Working on having buttons checked, and which mode is currently active

---
+++ javascript.js
@@ -0,0 +1,36 @@
+function buildGrid(size) {
+    const grid = document.querySelector(".grid");
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement("div");
+            cell.className = "cell";
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+}
+buildGrid(10);
+
+let isDrawing = false;
+const circle = document.querySelector("#color");
+const grid = document.querySelectorAll(".cell");
+grid.forEach((cell) => {
+    cell.addEventListener("mousedown", () => {
+        isDrawing = true;
+        cell.style.backgroundColor = circle.value;
+    });
+    cell.addEventListener("mouseup", () => {
+        isDrawing = false;
+    });
+    cell.addEventListener("mouseover", () => {
+        if (isDrawing) {
+            cell.style.backgroundColor = circle.value;
+        }
+    });
+});
+
+const selectors = document.querySelectorAll(".selector");
+
+
